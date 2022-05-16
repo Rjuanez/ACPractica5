5a22222cc27dea23e5021afe6ba369bc88f9543f
Ruben <ruben@netvn-1.home>
2022-05-16 21:55:54 +0200

    .

--- P10/Practica10/Procesar_unal.s
+++ P10/Practica10/Procesar_unal.s
@@ -11,32 +11,7 @@ procesar:
 	pushl	%edi
 
 # Aqui has de introducir el codigo
-movl $0, %esi        #%esi(=i) = 0
-    movl 8(%ebp), %ebx    #%ebx = *mata
-    movl 12(%ebp), %edi    #%edi = *matb
-    movl 16(%ebp), %ecx    #%ecx = n
-    
-    imul %eax, %eax        #eax = n²
-_IniFor:
-    cmpl %eax, %esi        #%i < n² -> en un únic bucle
-    jge _FiFor        #saltem quan i >= n²
-    movb (%ebx), %dl    #dl = mata[i*n+j] <- volem part baixa
-    andb $1, %dl    #dl = mata[i*n+j]  & 1
-    movb %dl, (%edi)    #matb[i*n+j] = %dl <- tractem amb chars
-_IniIf:
-    cmpb $0, (%edi)        #matb[i*n+j] > 0
-    jle _IniElse        #saltem quan matc[i*n+j] <= 0
-    movb $0, (%edi)    #matb[i*n+j] = FF
-    jmp _Fi
-_IniElse:
-    movb $255, (%edi)        #matb[i*n+j] = 0
-_Fi:
-    incl %esi        #i+=1
-    incl %ebx        #nem al següent element amb chars i seq
-    incl %edi
-    jmp _IniFor
 
-_FiFor:
 
 # El final de la rutina ya esta programado
 
