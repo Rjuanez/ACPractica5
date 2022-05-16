c62724e61b09322208f8b412fd0bbf397c01230d
Ruben <ruben@netvn-1.home>
2022-05-16 23:00:53 +0200

    .

--- P10/Practica10/Procesar_asm.s
+++ P10/Practica10/Procesar_asm.s
@@ -11,39 +11,39 @@ procesar:
     pushl    %edi
 
 # Aqui has de introducir el codigo
-
     movl $0, %esi        #%esi(=i) = 0
     movl 8(%ebp), %ebx    #%ebx = *mata
     movl 12(%ebp), %edi    #%edi = *matb
-    movl 16(%ebp), %ecx    #%ecx = *matc
-    movl 20(%ebp), %eax    #%eax = n
+    movl 16(%ebp), %ecx    #%ecx = n
     
     imul %eax, %eax        #eax = n²
-_IniFor:
-    cmpl %eax, %esi        #%i < n² -> en un únic bucle
-    jge _FiFor        #saltem quan i >= n²
-    movb (%ebx), %dl    #dl = mata[i*n+j] <- volem part baixa
-    subb (%edi), %dl    #dl = mata[i*n+j] - matb[i*n+j]
-    movb %dl, (%ecx)    #matc[i*n+j] = %dl <- tractem amb chars
-_IniIf:
-    cmpb $0, (%ecx)        #matc[i*n+j] > 0
-    jle _IniElse        #saltem quan matc[i*n+j] <= 0
-    movb $255, (%ecx)    #matc[i*n+j] = FF
+_For:
+    cmpl %eax, %esi        #%i < n²
+    jge _FiFor
+    movb (%ebx), %dl    #dl = mata[i*n+j]
+    andb $1, %dl        #dl = mata[i*n+j]  & 1
+    movb %dl, (%edi)    #matb[i*n+j] = %dl
+_If:
+    cmpb $0, (%edi)
+    jle _Else        #matb[i*n+j] <= 0
+    movb $255, (%edi)   #matb[i*n+j] = 255
     jmp _Fi
-_IniElse:
-    movb $0, (%ecx)        #matc[i*n+j] = 0
+_Else:
+    movb $0, (%edi)     #matb[i*n+j] = 0
 _Fi:
-    incl %esi        #i+=1
-    incl %ebx        #nem al següent element amb chars i seq
-    incl %ecx
+    incl %esi           #i+=1
+    incl %ebx
     incl %edi
-    jmp _IniFor
+    jmp _For
 
 _FiFor:
+
 # El final de la rutina ya esta programado
+
     popl    %edi
     popl    %esi
     popl    %ebx
     movl %ebp,%esp
     popl %ebp
+    ret
     ret
